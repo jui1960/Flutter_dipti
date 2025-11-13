179e482249622ffbb1fba9e018dad430975f4e04
jui1960 <juifull196044@gmail.com>
2025-11-13 23:31:41 +0600

    update

--- food_app/lib/main.dart
+++ food_app/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -107,7 +107,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:',style: TextStyle(color: Colors.red),),
+            const Text('App:',style: TextStyle(color: Colors.red),),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
@@ -117,7 +117,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        tooltip: 'Incre',
         child: const Icon(Icons.add),
       ),
     );
